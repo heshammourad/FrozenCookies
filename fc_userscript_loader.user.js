796d2e8dd8c91205ad4b847b9c8ba0c9eb1f04ca
Hesham Mourad <heshammourad@gmail.com>
2022-04-24 13:54:36 -0700

    Update fc_userscript_loader.user.js

--- fc_userscript_loader.user.js
+++ fc_userscript_loader.user.js
@@ -6,17 +6,17 @@
 // @homepage       https://github.com/icehawk78/FrozenCookies
 // @include        http://orteil.dashnet.org/cookieclicker/
 // @include        https://orteil.dashnet.org/cookieclicker/
-// @updateURL      https://icehawk78.github.io/FrozenCookies/fc_userscript_loader.user.js
-// @downloadURL    https://icehawk78.github.io/FrozenCookies/fc_userscript_loader.user.js
+// @updateURL      https://heshammourad.github.io/FrozenCookies/fc_userscript_loader.user.js
+// @downloadURL    https://heshammourad.github.io/FrozenCookies/fc_userscript_loader.user.js
 // ==/UserScript==
 
 // Source:    https://github.com/Icehawk78/FrozenCookies/main/
-// Github.io: http://icehawk78.github.io/FrozenCookies/
+// Github.io: http://heshammourad.github.io/FrozenCookies/
 
 var loadInterval = setInterval(function () {
   const Game = unsafeWindow.Game;
   if (Game && Game.ready) {
     clearInterval(loadInterval);
-    Game.LoadMod("https://icehawk78.github.io/FrozenCookies/frozen_cookies.js");
+    Game.LoadMod("https://heshammourad.github.io/FrozenCookies/frozen_cookies.js");
   }
 }, 1000);
